a579ba21b007009dfbe060810fd1030d9fcf5ac1
nina <nina880120@gmail.com>
2016-12-15 10:30:06 +0800

    1. Markdown笔记 2. Readme中python-概率更新

--- 201612/draw.py
+++ 201612/draw.py
@@ -27,7 +27,7 @@ def post_https(base_url, url):
 
 def pro_prize(list_price):
     '''
-    :param list_price: 统计抽奖次数，为二位数组
+    :param list_price: 统计抽奖次数，为二维数组
     :return: 抽奖概率
     '''
     for j in range(0, len(list_price)):
@@ -45,7 +45,7 @@ def simulate_draw(base_url, draw_url, num, list_prize_id):
     :param draw_url: 抽奖url
     :param num: 模拟num轮抽奖，每轮抽奖的次数(1, 1000)随机产生
     :param list_prize_id: 奖品id列表,例如 [1, 2, 3]
-    :return: 返回抽奖次数列表，为二位数组
+    :return: 返回抽奖次数列表，为二维数组
     '''
     prize_num = len(list_prize_id)
     list_total = [[0] * prize_num]*num  # 初始化抽奖次数
