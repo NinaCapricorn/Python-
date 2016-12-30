14c3532bbd6d4c500da76605723fb0044ee21814
nina <nina880120@gmail.com>
2016-12-30 19:48:48 +0800

    add

--- Python基础教程学习笔记/第二章 列表和元组/2.3_list.py
+++ Python基础教程学习笔记/第二章 列表和元组/2.3_list.py
@@ -37,3 +37,71 @@ print list('Hello')
 # 列表转字符串
 print ''.join(['h', 'o', 'o'])
 
+# 赋值、删除
+names = ['Alica', 'April', 'Beth', 'Pencil']
+del names[0]
+names[1] = []
+print names
+
+name = list('Capricorn')
+name[3:7] = list('tern4')  # 分片赋值
+print name
+name[:3] = []  # 删除
+print name
+name[1:1] = list('INSERT')  # 插入
+print name
+
+num = list([0, 2, 3, 5, 10])
+num.append(5)
+nums_add = [6, 7, 8]
+num.extend(nums_add)
+print num
+nums_add[len(nums_add)+1:] = [0, 0, 0]
+print nums_add
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+a[:] = a + b
+print a
+print b
+
+nums = [1, 2, [1, 2], 4, 1]
+print nums.count(1)
+print nums.index(1)
+
+nums = [1, 2, 3, 4]
+nums.insert(0, 5)
+nums[1:1] = [6, 7]
+print nums
+
+
+x = [1, 2, 3]
+element_last = x.pop()  # 默认为最后一个元素
+e = x.pop(0)  # 移除第一个元素
+x.append(x.pop())
+print x
+
+
+x = [1, 2, 3, 4, 1]
+x.remove(1)
+print x
+
+x = [1, 2, 3]
+x.reverse()
+y = list(reversed(x))
+print x
+print y
+print x
+
+
+nums = [5, 2, 9, 7]
+nums.sort(cmp)
+print nums
+
+
+names = ['Apple', 'Pear', 'Banana', 'Durian', 'Peach']
+names.sort(key=len, reverse=True)
+print names
+
+t = (12,)
+print t
